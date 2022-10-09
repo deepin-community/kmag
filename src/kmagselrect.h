1fe36c963089af07c33123ae5b1307ea329fef4e
Deepin Developer <deepin-dev@deepin.org>
2022-10-09 16:31:25 +0800

    feat: Init commit
    init kmag
    
    Log:

---
+++ src/kmagselrect.h
@@ -0,0 +1,143 @@
+/***************************************************************************
+                          kmagselrect.h  -  description
+                             -------------------
+    begin                : Mon Feb 12 23:45:41 EST 2001
+    copyright            : (C) 2001-2003 by Sarang Lakare
+    email                : sarang#users.sf.net
+    copyright            : (C) 2003-2004 by Olaf Schmidt
+    email                : ojschmidt@kde.org
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef KMAGSELRECT_H
+#define KMAGSELRECT_H
+
+// Qt
+#include <QWidget>
+#include <QLabel>
+
+class QMouseEvent;
+
+class KMagSelWinCorner : public QLabel
+{
+    Q_OBJECT
+
+public:
+
+    explicit KMagSelWinCorner ( QWidget * parent = nullptr );
+
+    virtual ~KMagSelWinCorner();
+
+Q_SIGNALS:
+
+    void startResizing ();
+    void resized ( QPoint offset );
+
+protected:
+
+    QPoint oldPos;
+
+    void mousePressEvent ( QMouseEvent * e ) override;
+    void mouseReleaseEvent ( QMouseEvent * e ) override;
+    void mouseMoveEvent ( QMouseEvent * e ) override;
+};
+
+class KMagSelWin : public QWidget
+{
+    Q_OBJECT
+
+public:
+
+    explicit KMagSelWin ( QWidget * parent = nullptr );
+
+    virtual ~KMagSelWin();
+
+    void setSelRect ( const QRect & selRect );
+    QRect getSelRect ();
+
+public Q_SLOTS:
+
+    void startResizing ();
+    void titleMoved ( const QPoint & offset );
+    void topLeftResized ( const QPoint & offset );
+    void topRightResized ( const QPoint & offset );
+    void bottomLeftResized ( const QPoint & offset );
+    void bottomRightResized ( const QPoint & offset );
+
+Q_SIGNALS:
+
+    void resized();
+
+protected:
+
+    QRect oldSelRect;
+
+    KMagSelWinCorner *titleBar;
+    KMagSelWinCorner *topLeftCorner;
+    KMagSelWinCorner *topRightCorner;
+    KMagSelWinCorner *bottomLeftCorner;
+    KMagSelWinCorner *bottomRightCorner;
+};
+
+/**
+ * This class stores the selected rectangular area for grabbing. It also displays the
+ * rectangular area on demand.
+ *
+ * @author Original : Michael Forster
+ * @author Current : Sarang Lakare
+ */
+class KMagSelRect : public QObject, public QRect
+{
+    Q_OBJECT
+
+public:
+    explicit KMagSelRect(QWidget *parent=nullptr);
+    KMagSelRect(const QPoint &topLeft, const QPoint &bottomRight,
+      QWidget *parent=nullptr);
+    KMagSelRect(const QPoint &topLeft, const QSize &size,
+      QWidget *parent=nullptr);
+    KMagSelRect(int left, int top, int width, int height,
+      QWidget *selWindowParent=nullptr);
+
+    virtual ~KMagSelRect();
+
+    bool visible() const;
+
+    /// Makes the rectangle always visible
+    void alwaysVisible(bool visible=true);
+
+    /// Returns true if always visible is set
+    bool getAlwaysVisible() const {
+      return (m_alwaysVisible);
+    }
+
+public Q_SLOTS:
+
+    void show();
+    void hide();
+    void update();
+
+    void selWinResized();
+
+protected:
+
+    void init(QWidget *);
+
+    QWidget *selWindowParent;
+    KMagSelWin *selectionwindow;
+    bool m_alwaysVisible;
+
+};
+
+void setTitleColors (const QColor & title, const QColor & text, const QColor & titleBtn);
+void setFrameSize (int size);
+
+#endif // KMAGSELRECT_H
---
+++ src/main.cpp
@@ -0,0 +1,78 @@
+/***************************************************************************
+                          main.cpp  -  description
+                             -------------------
+    begin                : Mon Feb 12 23:45:41 EST 2001
+    copyright            : (C) 2001-2003 by Sarang Lakare
+    email                : sarang#users.sf.net
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+// Qt
+#include <QApplication>
+#include <QCommandLineParser>
+
+// KF5
+#include <KAboutData>
+#include <KLocalizedString>
+
+#include "kmag.h"
+
+#include "kmag_version.h"
+
+KmagApp *kmagapp;
+
+// Not needed, not used.
+//static const char description[] =
+//  I18N_NOOP("Kmag");
+// INSERT A DESCRIPTION FOR YOUR APPLICATION HERE
+
+
+int main(int argc, char *argv[])
+{
+  QApplication app(argc, argv);
+  app.setAttribute(Qt::AA_UseHighDpiPixmaps, true);
+  KLocalizedString::setApplicationDomain("kmag");
+
+  // about the application
+  KAboutData aboutData(QStringLiteral("kmag"), i18n("KMagnifier"), QStringLiteral(KMAG_VERSION_STRING),
+                                         i18n("Screen magnifier created by KDE"),
+                                         KAboutLicense::GPL,
+                                         i18n("Copyright 2001-2003 Sarang Lakare\nCopyright 2003-2004 Olaf Schmidt\nCopyright 2008 Matthew Woehlke"), QString(),
+                                         QStringLiteral("https://accessibility.kde.org/"));
+
+  // about the authors
+  aboutData.addAuthor(i18n("Sarang Lakare"),
+                       i18n("Rewrite"),QStringLiteral("sarang@users.sf.net"));
+  aboutData.addAuthor(i18n("Michael Forster"),
+                       i18n("Original idea and author (KDE1)"), QStringLiteral("forster@fmi.uni-passau.de"));
+  aboutData.addAuthor(i18n("Olaf Schmidt"), i18n("Rework of the user interface, improved selection window, speed optimization, rotation, bug fixes"), QStringLiteral("ojschmidt@kde.org"));
+  aboutData.addCredit(i18n("Matthew Woehlke"), i18n("Color-blindness simulation"), QStringLiteral("mw_triad@users.sourceforge.net"));
+  aboutData.addCredit(i18n("Sebastian Sauer"), i18n("Focus tracking"), QStringLiteral("sebsauer@kdab.com"));
+  aboutData.addCredit(i18n("Claudiu Costin"), i18n("Some tips"), QStringLiteral("claudiuc@work.ro"), QStringLiteral("https://ro.kde.org"));
+
+  KAboutData::setApplicationData(aboutData);
+
+  if (app.isSessionRestored())
+  {
+    kRestoreMainWindows<KmagApp>();
+  }
+  else
+  {
+    QCommandLineParser parser;
+    aboutData.setupCommandLine(&parser);
+    parser.process(app);
+
+    kmagapp = new KmagApp();
+    kmagapp->show();
+  }
+
+  return app.exec();
+}
